a3feec6a8fa215877e1690c86f25cac27f5576a7
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-04 21:52:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
 
-def countdown(x)
-  while x > 0
-    return "HAPPY NEW YEAR"
+def countdown(number)
+  while number > 0
+    return "#{number} SECOND(S)!"
     x -= 1
+    return "HAPPY NEW YEAR"
   end
 end
