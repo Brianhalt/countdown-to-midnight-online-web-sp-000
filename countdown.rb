f59ee472a288b8c66856e7d31517f8f525609d8b
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-04 22:00:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   puts "HAPPY NEW YEAR"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    sleep(1.second)
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR"
+end
