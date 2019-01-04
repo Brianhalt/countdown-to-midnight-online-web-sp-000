36b0f4ff56d20dee4def64ec5b20a1569ddbbcd7
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-04 22:01:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,17 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR"
+  return "HAPPY NEW YEAR"
 end
 
 def countdown_with_sleep(number)
   while number > 0
     sleep(1.second)
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR"
+  return "HAPPY NEW YEAR"
 end
